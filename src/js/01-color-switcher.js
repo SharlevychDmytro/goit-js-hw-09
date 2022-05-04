6083fda46cbc9683d4a03d078e42d75002a247f9
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-05-05 00:19:57 +0300

    add task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  startBtnEl: document.querySelector('button[data-start]'),
+  stopBtnEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+refs.stopBtnEl.setAttribute('disabled', '');
+
+refs.startBtnEl.addEventListener('click', onClickStart);
+refs.stopBtnEl.addEventListener('click', onClickstop);
+
+function onClickStart() {
+  console.log('start');
+  refs.startBtnEl.setAttribute('disabled', '');
+  refs.stopBtnEl.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    changeBackroundColor(getRandomHexColor());
+  }, 1000);
+}
+
+function onClickstop() {
+  console.log('stop');
+  refs.startBtnEl.removeAttribute('disabled');
+  refs.stopBtnEl.setAttribute('disabled', '');
+  clearInterval(intervalId);
+}
+
+function changeBackroundColor(callback) {
+  refs.bodyEl.style.backgroundColor = `${callback}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
